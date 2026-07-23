6e699f6781ca3ae3ab911d1414feeb64ad08091e
pavelev.daniil <pavelev.daniil@rwb.ru>
2026-07-23 22:33:19 +0300

    init

---
+++ src/main/java/daniil/pavelev/PavelevApplication.java
@@ -0,0 +1,13 @@
+package daniil.pavelev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavelevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PavelevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daniil/pavelev/PavelevApplicationTests.java
@@ -0,0 +1,13 @@
+package daniil.pavelev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavelevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
